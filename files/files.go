1f3f286b60618479a811384151d821237b0696ef
Warisamir <warisamir1918@gmail.com>
2025-04-22 10:59:04 +0530

    adding readfile

--- files/files.go
+++ files/files.go
@@ -12,17 +12,22 @@ func main() {
 	// 	panic(err)
 	// }
 	// fileInfo,err := f.Stat()
-	f,err:= os.Open("file.txt")
-	if err!= nil{
-		panic (err)
-	}
-	defer f.Close()
+	// f,err:= os.Open("file.txt")
+	// if err!= nil{
+	// 	panic (err)
+	// }
+	// defer f.Close()
 
-	buf := make([]byte, 10)
-	d,err:= f.Read(buf)
-	if err!=nil{ panic(err)
-	}
-	for i:=0 ;i<len(buf);i++{
-	fmt.Println("Data",d, string(buf[i]))
-	}
+	// buf := make([]byte, 10)
+	// d,err:= f.Read(buf)
+	// if err!=nil{ panic(err)
+	// }
+	// for i:=0 ;i<len(buf);i++{
+	// fmt.Println("Data",d, string(buf[i]))
+	// }
+	// data,err:= os.ReadFile("file.txt")
+	// if err!=nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(string (data))
 }
