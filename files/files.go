80219de8c514f4b075f970b5abc19efcb49cde6e
Warisamir <warisamir1918@gmail.com>
2025-04-22 10:56:44 +0530

    adding files operations

---
+++ files/files.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+  	// f,err:=os.Open("file.txt")
+	// if err!=nil{
+	// 	//log error
+	// 	panic(err)
+	// }
+	// fileInfo,err := f.Stat()
+	f,err:= os.Open("file.txt")
+	if err!= nil{
+		panic (err)
+	}
+	defer f.Close()
+
+	buf := make([]byte, 10)
+	d,err:= f.Read(buf)
+	if err!=nil{ panic(err)
+	}
+	for i:=0 ;i<len(buf);i++{
+	fmt.Println("Data",d, string(buf[i]))
+	}
+}
