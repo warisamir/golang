754dca93e39ad2a0296e2a9376842d95118a6009
Warisamir <warisamir1918@gmail.com>
2025-04-23 01:47:51 +0530

    adding project config

--- Project/internal/config/config.go
+++ Project/internal/config/config.go
@@ -4,19 +4,21 @@ import (
 	"flag"
 	"log"
 	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
 	Addr string
 }
-
+//  env-default:"production"
 type Config struct {
 	Env         string `yaml:env env:"ENV" env-required:"true"`
 	StoragePath string `yaml:"storage_path" env-required:true`
 	HTTPServer  `yaml:"http_server"`
 }
 
-func mustLoad() {
+func MustLoad() *Config{
 	var configpath string
 	configpath= os.Getenv("CONFIG_PATH")
 
@@ -32,4 +34,10 @@ func mustLoad() {
 	if _,err:= os.Stat(configpath); os.IsNotExist(err){
 		log.Fatalf("confing file not exist: %s",configpath)
 	}
+	var cfg Config
+	err:= cleanenv.ReadConfig(configpath, &cfg)
+	if err !=nil{
+		log.Fatalf("cannot read config file : %s",err.Error())
+	}
+	return &cfg;
 }
